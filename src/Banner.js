29df72e1aed46221961d8085c78932aa16422701
noorulameenkm <noorul.km@people10.com>
2020-08-11 16:25:19 +0530

    Initial and final commit

---
+++ src/Banner.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import requests from "./requests";
+import "./banner.css";
+
+function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(requests.fetchNetflixOriginals);
+
+      const randomIndex = Math.floor(
+        Math.random() * response.data.results.length - 1
+      );
+
+      const selectedMovie = response.data.results[randomIndex];
+
+      setMovie(selectedMovie);
+    }
+
+    fetchData();
+  }, []);
+
+  function truncate(string, n) {
+    return string?.length > n ? string.substr(0, n - 1) + "..." : string;
+  }
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner__contents">
+        {/* title */}
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        {/* div > 2 buttons */}
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        {/*  description */}
+        <h1 className="banner__description">
+          {truncate(movie?.overview, 150)}
+        </h1>
+      </div>
+      <div className="banner__fadeBottom"></div>
+    </header>
+  );
+}
+
+export default Banner;
